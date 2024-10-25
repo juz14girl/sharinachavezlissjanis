12a9ce5fee0cc415fe36cf1056e31f0ac106538b
Sharina Chavez-Lissjanis <sha.cha016@gmail.com>
2024-10-25 22:13:09 +0200

    removed the print statement of greeting the user in Guesser.java

--- Guesser.java
+++ Guesser.java
@@ -63,10 +63,6 @@ public class Guesser {
         // As long as it is not a valid reply (one of "T" and "F")
         // write an error message, and read a new reply.
         // When you have gotten a valid reply, return it.
-        System.out.print("Enter your name: ");
-        String name = System.console().readLine();
-
-        System.out.printf("Hi %s, today we're going to have a guessing game", name);
         
         System.out.println("Please enter 'T' for true and 'F' for false");
         String reply = System.console().readLine();
